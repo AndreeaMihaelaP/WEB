7c2e4bec532052dfe92c03a606a3162a3728f0ad
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2021-09-21 19:03:23 +0300

    Started javascript

---
+++ Javascript/basic.js
@@ -0,0 +1,124 @@
+//  JAVASCRIPT TYPES
+//  -----------------
+
+// 1. Number
+
+console.log(3 + 4); // 7
+console.log(12 - 4); // 8
+console.log(3 * 5); // 15
+console.log(12 / 4); // 3
+console.log((3 + 4) * 2); // 14
+console.log(12 % 6); // 0
+console.log(12 % 5); // 2
+
+// 2. String
+
+console.log("Bob"); // "Bob"
+console.log("Hello" + "There"); // "HelloThere"
+console.log("Hello " + "there"); // "Hello there"
+console.log("This isn't very nice"); // "This isn't very nice"
+console.log("This isn't very nice"); // "This isn't very nice"
+console.log(10 + "34"); // "1034"
+console.log(10 - "3"); // 7 - but is number
+console.log("hello" - "bye"); // NaN - not a number
+
+// 3. Boolean
+
+console.log(true); // true
+console.log(false); // false
+console.log(3 > 2); // true
+console.log(5 > 10); // false
+console.log(5 >= 5); // true
+console.log(5 <= 5); // true
+console.log(3 === 3); // true
+console.log(3 !== 3); // false
+
+// 4. Undefined
+// 5. Null
+// <!-- 6. Symbol (new in ECMAScript 6) -->
+// 7. Object
+
+// JAVASCRIPT COMPARISONS
+// -----------------
+
+// !==
+// ===
+// >=
+// <=
+// >
+// <
+
+// JAVASCRIPT VARIABLES
+// -----------------
+// var
+// <!-- let (new in ECMAScript 6)-->
+// <!-- const (new in ECMAScript 6)-->
+
+// JAVASCRIPT CONDITIONALS
+// -----------------
+// if
+// else
+// else if
+// <!-- ternary operator -->
+// <!-- switch -->
+
+// JAVASCRIPT LOGICAL OPERATORS
+// -----------------
+// &&
+// ||
+// !
+
+// JAVASCRIPT FUNCTIONS
+// -----------------
+// var a = function name() {}
+// function name() {}
+// return
+// <!-- () => (new in ECMAScript 6) -->
+
+// JAVASCRIPT DATA STRUCTURES
+// -----------------
+// Array
+// Object
+
+// JAVASCRIPT LOOPING
+// -----------------
+// for
+// while
+// do
+// forEach (new in ECMAScript 5)
+
+// JAVASCRIPT KEYWORDS
+// -----------------
+// break
+// case
+// catch
+// class
+// const
+// continue
+// debugger
+// default
+// delete
+// do
+// else
+// export
+// extends
+// finally
+// for
+// function
+// if
+// import
+// in
+// instanceof
+// new
+// return
+// super
+// switch
+// this
+// throw
+// try
+// typeof
+// var
+// void
+// while
+// with
+// yield
