de8e083ced50991ae0f514c8d5e1744ef8fd9a56
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2021-09-24 17:44:30 +0300

    Added an exercise with events DOM

--- DOM/Concepts/events.js
+++ DOM/Concepts/events.js
@@ -15,9 +15,18 @@ function inputLength() {
 
 function createListElement() {
   var li = document.createElement("li");
+  var deleteButton = document.createElement("button");
+  var br = document.createElement("br");
+
+  deleteButton.id = "delete";
   li.appendChild(document.createTextNode(input.value));
+  deleteButton.appendChild(document.createTextNode("Delete"));
   ul.appendChild(li);
+  li.appendChild(deleteButton);
+  ul.appendChild(br);
   input.value = "";
+  deleteElement();
+  toggleElement();
 }
 
 function addListAfterClick() {
@@ -31,6 +40,34 @@ function addListAfterKeyPress(event) {
     createListElement();
   }
 }
+
+// If you click on the list item, it toggles the .done class on and off.
+function toggleElement() {
+  var listItems = document.querySelectorAll("li");
+  listItems.forEach(function (element) {
+    element.addEventListener("click", function () {
+      element.classList.toggle("done");
+    });
+  });
+}
+
+// Add buttons next to each list item to delete the item when clicked on its corresponding delete button.
+function deleteElement() {
+  var deleteButtons = document.querySelectorAll("#delete");
+  var br = document.querySelectorAll("br");
+  var ul = document.querySelector("ul");
+
+  deleteButtons.forEach(function (element, index) {
+    element.addEventListener("click", function () {
+      const li = element.parentNode;
+      ul.removeChild(li);
+      ul.removeChild(br[index]);
+    });
+  });
+}
+
+deleteElement();
+toggleElement();
 
 buttonEnter.addEventListener("click", addListAfterClick);
 
