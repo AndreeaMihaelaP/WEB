a9202d5bac165854b2922fa88aedffed1e8d599f
Andreea <53224914+AndreeaMihaelaP@users.noreply.github.com>
2021-11-11 13:34:27 +0000

    Update async-await.js

--- Javascript/Asyncronous/async-await.js
+++ Javascript/Asyncronous/async-await.js
@@ -1,1 +1,9 @@
 
+// async-await return a promise
+// easier to read
+
+async function playerStart () {
+  const firstMove = await movePlayer(100, "left");
+  await movePlayer(200, "left");
+  await movePlayer(300, "right");
+  await movePlayer(400, "left");
